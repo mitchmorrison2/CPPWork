a3701e596cc955feaca40a4607d201afcb1d658d
mitchmorrison2 <47828551+mitchmorrison2@users.noreply.github.com>
2019-08-29 22:47:56 -0500

    Update mString.h

--- StringClass/mString.h
+++ StringClass/mString.h
@@ -3,35 +3,36 @@
 
 class mString
 {
-	
-private: 
-	char* data;
-	
+
+private:
+    char* data;
+
 public:
-	
-	mString();
-	mString(char* data);
-	mString(const mString&);
-	~mString();
-	
-	mString& operation= (const char*);
-	mString& operation= (const mString&);
-	mString operation+ (const mString&);
-	mString& operation+= (const mString&);
-	bool operator== (const char*);
-	bool operator==(const mString&);
-	bool operator< (const char*);
-	bool operator< (const mString&);
-	
-	char& operator[] (const int);
-	
-	int size();
-	
-	mString substring(int, int);
-	
-	char* c_str();
-	
-	friend std::ostream& operator<< (std::ostream&, const mString&);
+
+    mString();
+    mString(char* data);
+    mString(const mString&);
+    ~mString();
+
+    mString& operator= (const char*);
+    mString& operator= (const mString&);
+    mString operator+ (const mString&);
+    mString& operator+= (const mString&);
+    bool operator== (const char*);
+    bool operator==(const mString&);
+    bool operator< (const char*);
+    bool operator< (const mString&);
+
+    char& operator[] (const int);
+
+    int size();
+
+    mString substring(int, int);
+
+    char* c_str();
+
+//	friend std::ostream& operator<< (std::ostream&, const mString&);
 };
 
 #endif // MSTRING_H
+
