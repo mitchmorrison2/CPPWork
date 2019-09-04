7c0a20aebbbd0f65c8a2cec0ae3168116c226520
mitchmorrison2 <47828551+mitchmorrison2@users.noreply.github.com>
2019-09-03 21:30:40 -0500

    Update mString.cpp

--- StringClass/mString.cpp
+++ StringClass/mString.cpp
@@ -1,5 +1,6 @@
 #include "mstring.h"
 #include <cstring>
+#include <iostream>
 
 mString::mString()
 {
@@ -24,7 +25,7 @@ mString::mString(const mString& p) {
 
 mString& mString::operator= (const char* p) {
     if (data!= nullptr) {
-        delete[] this->data; //invoking data member
+        delete[] data; //invoking data member deleted this
     }
     data = new char[strlen(p) + 1];
     strcpy(data, p);
@@ -32,16 +33,13 @@ mString& mString::operator= (const char* p) {
 }
 
 mString& mString::operator= (const mString& p) {
-    if (data != nullptr) {
-        delete[] this->data;
-    }
+    if (data != nullptr) delete[] this->data;
     data = new char[strlen(p.data) + 1];
     strcpy(data, p.data);
     return (*this); //returns address of new mString copy
 }
 
 mString mString::operator+ (const mString& z) {
-
     char* temp = this->data;
     temp = new char[strlen(data) + strlen(z.data) + 1];
     strcpy(temp, data);
@@ -51,9 +49,7 @@ mString mString::operator+ (const mString& z) {
 
 mString& mString::operator+= (const mString& z) {
     char* temp = this->data;
-    if (data != nullptr) {
-        delete[] this->data; //free memory for usage
-    }
+    if (data != nullptr) delete[] this->data; //free memory for usage
     this->data = new char[strlen(data) + strlen(z.data) + 1];
     strcpy(this->data, temp);
     strcat(this->data, z.data);
@@ -62,50 +58,50 @@ mString& mString::operator+= (const mString& z) {
 }
 
 bool mString::operator== (const char* t) {
-    if (strcmp(data, t) != 0) { //strcmp evaluates to 0 if equal
-        return false;
-    }
+    if (strcmp(data, t) != 0) return false;
     else return true;
-}
+} //strcmp evaluates to 0 if equal
 
 bool mString::operator== (const mString& p) {
-    if (strcmp(data, p.data) != 0) { //strcmp evaluates to 0 if equal
-        return false;
-    }
+    if (strcmp(data, p.data) != 0) return false;
     else return true;
-}
+} //strcmp evaluates to 0 if equal
 
 bool mString::operator< (const char* s) {
-    if (strcmp(this->data, s) < 0) { //if strcmp evaluates to <0 returns true
-        return true;
-    }
+    if (strcmp(this->data, s) < 0) return true;
     else return false;
-}
+} //if strcmp evaluates to <0 returns true
 
 bool mString::operator< (const mString& s) {
-        if (strcmp(this->data, s.data) < 0) { //if strcmp evaluates to <0 returns true
-        return true;
-    }
+    if (strcmp(this->data, s.data) < 0) return true;
     else return false;
 }
 
 char& mString::operator[] (const int n) {
-
+    if (n >= int(strlen(data))) throw ("Out of range"); //throw the exception
+    return data[n];
 }
 
-int mString::size() {
-
+int mString::size() { //return size of mString
+    //how do I keep the size constant? I don't want to recalculate it every time
+    return int(strlen(data));
 }
 
-mString mString::substring(int a, int b) {
-
+mString mString::substring(int startingPoint, int lengthAndDirection) {
+    mString copy;
+    if(lengthAndDirection > 0) strncpy(copy.data, &data[startingPoint], unsigned (lengthAndDirection));
+    else strncpy(copy.data, &data[startingPoint - lengthAndDirection], unsigned (lengthAndDirection));
+    return copy;
 }
 
-char c_str() {
+char* mString::c_str() {
+    return data;
+} //returns address of 1st array element
 
+
+std::ostream& operator<< (std::ostream& output, const mString& p) { //should this be mString
+    output << p.data;
+    return output;
 }
 
-friend std::ostream& operator<< (std::ostream&, const String&) { //should this be mString
-
-}
 
