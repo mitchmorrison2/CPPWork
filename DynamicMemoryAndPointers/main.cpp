999acd70d6dc2557c336b090e63eb2c0916367e2
mitchmorrison2 <47828551+mitchmorrison2@users.noreply.github.com>
2019-10-16 09:21:49 -0500

    Update main.cpp

--- DynamicMemoryAndPointers/main.cpp
+++ DynamicMemoryAndPointers/main.cpp
@@ -2,21 +2,26 @@
 
 using namespace std;
 
+
+//creates new array of all multiples of all elements
 int *apply_all (int array1[], size_t size1, int array2[], size_t size2);
 void printFunction(int *newArrayPtr, int endSize);
 
 int main (void) {
 
+//initializing first and second arrays
 int ar1[] {13, 23, 99, 1, 7};
 int ar2[] {10, 20, 72};
 size_t ar1size = sizeof(ar1) / sizeof(int); 
 size_t ar2size = sizeof(ar2) / sizeof(int);
+//size of new array
 int totalSize = ar1size * ar2size;
 
 int *newArray {nullptr};
 newArray = apply_all(ar1, ar1size, ar2, ar2size);
 printFunction(newArray, totalSize);
 
+//keep from memory leak
 delete [] newArray;
 
    return 0;
